24891d07b7c1f62a2ab9ea9ae379079524eb6c54
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-06-01 12:40:25 +0200

    forge setup, to be replaced with drone's

--- test/CaptainHappyHook.t.sol
+++ test/CaptainHappyHook.t.sol
@@ -2,8 +2,19 @@
 pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
+import "../src/CaptainHappyHook.sol";
 
+contract CaptainHappyHookTest is Test {
+    CaptainHappyHook public happy;
 
-contract NoOpSwapTest is Test {
-    
+    function test_swap() public {
+        happy = new CaptainHappyHook();
+        happy.beforeSwap( , , ,);
+    }
 }
+
+// beforeSwap(
+//         address,
+//         PoolKey calldata,
+//         IPoolManager.SwapParams calldata params,
+//         bytes calldata
