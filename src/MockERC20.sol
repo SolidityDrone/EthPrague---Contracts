0619e6ae3ff53af2cb465f5ca362212e4a7eb223
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-06-01 13:07:17 +0200

    original noop

--- src/MockERC20.sol
+++ src/MockERC20.sol
@@ -4,10 +4,8 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MockERC20 is ERC20{
-    constructor(string memory name, string memory symbol)ERC20(name, symbol)
-    {
-      _mint(msg.sender, 1000000000000 * 10 ** decimals());
+contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 1000000000000 * 10 ** decimals());
     }
-    
 }
